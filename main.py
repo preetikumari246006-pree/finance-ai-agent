69485cc795abe421b95faf1ea38d859b7de2cc89
PREETI KUMARI <preetikumari.246006@gmail.com>
2026-05-06 17:53:22 +0500

    Add Tracker and main function for user interaction

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-
+from tracker import Tracker
+def main():
+    tracker = Tracker()
+    print("Welcome!")
+    while True:
+        user_input = input("> ")
+        if user_input == "quit":
+            break
+if __name__ == "__main__":
+    main()
