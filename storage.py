b03bf1779a8e6d287a34ec602562dfb01b2fd567
PREETI KUMARI <preetikumari.246006@gmail.com>
2026-05-06 17:16:27 +0500

    Add methods to load and save expenses data
    Implement load_data and save_data methods for JSON file.

--- storage.py
+++ storage.py
@@ -1,8 +1,12 @@
 import json
 
 class Storage:
+    def __init__(self):
+        self.file = "data/expenses.json"
     def load_data(self):
-        pass
-    
-    def save_data(self):
-        pass
+        with open(self.file, "r") as f:
+            return json.load(f)
+        
+    def save_data(self, expenses):
+        with open(self.file, "w") as f:
+            json.dump(expenses, f)
