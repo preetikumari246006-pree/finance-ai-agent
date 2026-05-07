ae7eb805ca4d0b820cde1134c4ef5babd85d2235
PREETI KUMARI <preetikumari.246006@gmail.com>
2026-05-07 15:52:34 +0500

    Add Agent class with Tracker initialization

--- agent.py
+++ agent.py
@@ -1,1 +1,12 @@
+from tracker import Tracker
+import os
 
+class Agent:
+    def __init__(self):
+        self.tracker = Tracker()
+    
+    def run(self):
+        pass
+    
+    def process(self, user_input):
+        pass
